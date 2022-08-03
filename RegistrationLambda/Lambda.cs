5985485a5bc0619ac6c4f481b17a0626aa0e1408
Artem_Krol <artemkrol47@gmail.com>
2022-08-03 17:15:25 +0300

    update json file

--- RegistrationLambda/Lambda.cs
+++ RegistrationLambda/Lambda.cs
@@ -7,26 +7,27 @@ using System.Text.Json.Nodes;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace RegistrationLambda;
-
-public class Lambda
+namespace RegistrationLambda
 {
-    public static async Task<JsonObject?> FunctionHandler(InputEntity entity)
+    public static class Lambda
     {
-        var client = new HttpClient();
+        public static async Task<JsonObject?> FunctionHandler(InputEntity entity)
+        {
+            var client = new HttpClient();
 
-        if (entity.Body == null)
-            throw new ArgumentNullException("Body");
+            if (entity.Body == null)
+                throw new ArgumentNullException("Body");
 
-        var content = new StringContent(entity.Body.ToJsonString(), Encoding.UTF8, "application/json");
+            var content = new StringContent(entity.Body.ToJsonString(), Encoding.UTF8, "application/json");
 
-        content.Headers.Add("x-trackbox-username", entity.Username);
-        content.Headers.Add("x-trackbox-password", entity.Password);
-        content.Headers.Add("x-api-key", entity.ApiKey);
+            content.Headers.Add("x-trackbox-username", entity.Username);
+            content.Headers.Add("x-trackbox-password", entity.Password);
+            content.Headers.Add("x-api-key", entity.ApiKey);
 
-        var response = await client.PostAsync(entity.Url, content);
-        var responseString = await response.Content.ReadAsStringAsync();
+            var response = await client.PostAsync(entity.Url, content);
+            var responseString = await response.Content.ReadAsStringAsync();
 
-        return string.IsNullOrEmpty(responseString) ? new JsonObject() : JsonSerializer.Deserialize<JsonObject>(responseString);
+            return string.IsNullOrEmpty(responseString) ? new JsonObject() : JsonSerializer.Deserialize<JsonObject>(responseString);
+        }
     }
 }
